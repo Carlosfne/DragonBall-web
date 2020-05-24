2b2c5aad584c4571ef43b5a45e3c0fde9cd23a73
unknown <carlos.junior@grupoicts.com.br>
2020-05-24 00:02:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import logo from './assets/images/logo.png'
+import api from './services/api'
+import data from './data.json'
 function App() {
+  const [ personagens, setPersonagens ] = useState([])
+
+  async function getPersonagens(){
+    const response = await api.get('character')        
+    setPersonagens(response.data)
+    console.log(response.data)
+  }
+  useEffect(() => {
+    getPersonagens()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt="logo" className='logo'/>
+      <h1>Consumo de API Dragon Ball Z</h1>
+      <h2>Desenvolvido por Carlos Eduardo</h2>
+      <div className="personagens">
+        {
+          data.map(personagem =>(
+            <div className='cards'>
+              <p>Personagem : <p className='value'>{personagem.name} </p> </p>
+              <p>Espécie: <p className='value'>{personagem.species}</p></p> 
+              <p>Planeta de Origem : <p className='value'>{personagem.originPlanet}</p> </p>
+              <p>Série: <p className='value'>{personagem.series}</p> </p>              
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
